21d226ab44b0cb85c09bb4fa599a0af640d8c1ce
Hsunight <sky172839465@gmail.com>
2017-12-31 12:45:24 +0800

    新增 JS doc

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ var player,
 
 init();
 
+/**
+ * 遊戲初始化數值
+ * 
+ */
 function init() {
     player = { 
         whiteSide: { key: 'white', chesses: [] }, 
@@ -18,6 +22,10 @@ function init() {
     console.log(isCanvasSupport);    
 }
 
+/**
+ * 畫出棋盤
+ * 
+ */
 (function createChessBoard() {
     var chessRow, 
         chessColumn, 
@@ -110,6 +118,11 @@ function init() {
     }
 })();
 
+/**
+ * 把新棋子放到棋盤上
+ * 
+ * @param {any} event 下棋事件
+ */
 function chess(event) {
     var target, player, chess;
     target = event.target;
@@ -127,13 +140,25 @@ function chess(event) {
         target.appendChild(chess);
         target.classList.add('selected');
         changeAttackPlayer();
+        lastChess = chess;
     }
 }
 
+/**
+ * 取得目前是哪個角色在下棋
+ * 
+ * @returns 現在下棋的角色
+ */
 function getPlayer() {
     return player[attackSide];
 }
 
+/**
+ * 用canvas新增棋子
+ * 
+ * @param {any} player 目前下棋的角色
+ * @returns {Element}
+ */
 function createCanvasChess(player) {
     var chessBox, chess;
     chessBox = document.createElement('canvas');
@@ -148,6 +173,12 @@ function createCanvasChess(player) {
     return chessBox;
 }
 
+/**
+ * 用div新增棋子
+ * 
+ * @param {any} player 目前下棋的角色
+ * @returns {Element}
+ */
 function createDivChess(player) {
     var chess;
     chess = document.createElement('div');
@@ -157,6 +188,10 @@ function createDivChess(player) {
     return chess;
 }
 
+/**
+ * 切換下棋角色
+ * 
+ */
 function changeAttackPlayer() {
     if (attackSide === 'whiteSide') {
         attackSide = 'blackSide';
@@ -165,12 +200,25 @@ function changeAttackPlayer() {
     }
 }
 
+/**
+ * 判斷瀏覽器是否支援HTML5 Canvas
+ * https://stackoverflow.com/questions/2745432/best-way-to-detect-that-html5-canvas-is-not-supported?answertab=active#tab-top
+ * 
+ * @returns {boolean} 
+ */
 function checkIsCanvasSupprot() {
     var canvas;
     canvas = document.createElement('canvas');
     return !!(canvas.getContext && canvas.getContext('2d'));
 }
 
+/**
+ * 判斷指定的class是不是存在classList裡
+ * 
+ * @param {any} list classList
+ * @param {any} className class
+ * @returns {boolean} 
+ */
 function checkClassExist(list, className) {
     var isClassExist, 
         i;
